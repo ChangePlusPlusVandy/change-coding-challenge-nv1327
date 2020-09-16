69c3428c6e366aad589e0b9314ad949af65cc83a
nv1327 <n.vitebsky1327@gmail.com>
2020-09-16 15:11:17 -0500

    Testing env variables

--- twitterproj/src/App.js
+++ twitterproj/src/App.js
@@ -6,6 +6,11 @@ import Navbar from "./components/navbar.component";
 import SelectionList from './components/selectionlist.component';
 import ScoreList from './components/scorelist.component';
 
+
+//For Postman OAuth1.0 was used --> add authorization data: "Request Headers" and "Encode the parameters in the Authorization Header" was checked
+//For GET requests, leave body blank
+//Tested with: "https://api.twitter.com/2/users/by?usernames=elonmusk,kanyewest"
+
 function App() {
   return (
     <Router>
--- twitterproj/src/components/navbar.component.js
+++ twitterproj/src/components/navbar.component.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component{
+/*
+<li className="navbar-item">
+<Link to="/" className="nav-link">Selections</Link>
+</li>
+<li className="navbar-item">
+<Link to="/score" className="nav-link">Score</Link>
+</li>*/
+
   render() {
     return(
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Tweeter</Link>
+      <nav className="navbar navbar-expand-lg">
+        <Link to="/" className="navbar-brand">MuskYe</Link>
+<img src="/assets/muskye.png"></img>
         <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Selections</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/score" className="nav-link">Score</Link>
-          </li>
+
         </ul>
         </div>
         </nav>
