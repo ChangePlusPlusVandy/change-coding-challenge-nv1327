f8675756554b3f08e1f957237e6392d79e2fd813
nv1327 <n.vitebsky1327@gmail.com>
2020-09-19 18:30:32 -0500

    pull and filter from API, then shuffle without calling API again works

--- twitterproj/src/components/selectionlist.component.js
+++ twitterproj/src/components/selectionlist.component.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import { Column, Row } from 'simple-flexbox';
+import { Column, Row } from 'simple-flexbox'; //Flexbox
 import ReactCSSTransitionGroup from 'react-transition-group'; // ES6
-import { TwitterMentionButton } from 'react-twitter-embed';
-import "typeface-montserrat";
-import Tweet from './tweet';
+import { TwitterMentionButton } from 'react-twitter-embed'; //Widget for Twitter mentions
+//import "typeface-montserrat";
+import Tweet from './tweet'; //import Tweet object
 
 export default class SelectionList extends Component implements Tweet{
 
+  //give the class a constructor to establish an object for the class and set values for it
   constructor(props) {
     super(props);
+    //declare state values so they can be rendered and changed
     this.state = {
-      elon: "",
-      kanye: "",
+      elonTweets: "",
+      kanyeTweets: "",
+      display: "",
     };
+
   }
 
+  handleElon = () => {
+      console.log('Elon');
+      this.handleCalc();
+    }
 
+  handleKanye = () => {
+      console.log('Kanye');
+      this.handleCalc();
+    }
 
-  async componentDidMount() {
-    let elonResult = await Tweet.getTweet("elonmusk");
+  async handleRefresh() {
+    let elonResult = await Tweet.getTweet("elonmusk"); //request all filtered tweets from Tweet object
     let kanyeResult = await Tweet.getTweet("kanyewest");
-    var res = elonResult[0].text;
-    this.setState({elon: res});
+    this.setState({elonTweets: elonResult});
+    this.setState({kanyeTweets: kanyeResult});
+    this.handleCalc();
   }
+
+  async handleCalc(){
+    let elonResult = this.state.elonTweets;
+    let kanyeResult = this.state.kanyeTweets;
+    var randomDigit = Math.floor(Math.random() * 2);
+    var res;
+    //console.log(randomDigit);
+    if(randomDigit == 0) {
+      var randomElonDigit = Math.floor(Math.random() * elonResult.length);
+      res = elonResult[randomElonDigit].text;
+    }
+    else if(randomDigit == 1) {
+      var randomKanyeDigit = Math.floor(Math.random() * kanyeResult.length);
+      res = kanyeResult[randomKanyeDigit].text;
+    }
+    this.setState({display: res});
+  }
+
+
+  async componentWillMount() {
+    this.handleRefresh();
+  }
+
 
 /*
 <blockquote className="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
@@ -39,15 +75,15 @@ export default class SelectionList extends Component implements Tweet{
 
         <Row horizontal="center">
 
-        <p>{this.state.elon}</p>
+        <p style={{ marginTop: 235, marginBottom: 235}}>{this.state.display}</p>
 
         </Row>
 
         <Row horizontal = "space-evenly">
 
         <img src="https://i2.wp.com/www.celebrity-cutouts.com/wp-content/uploads/2020/01/elon-musk-smile-celebrity-mask.png?resize=450%2C500&ssl=1" className="img-fluid" alt="Responsive"  width="80px"></img>
-          <button type="button" className="btn btn-outline-primary btn-lg col-md-4 mr-2 mt-4 mb-3">Elon</button>
-          <button type="button" className="btn btn-outline-primary btn-lg col-md-4 ml-2 mt-4 mr-2 mb-3">Kanye</button>
+          <button onClick={this.handleElon} type="button" className="btn btn-outline-primary btn-lg col-md-4 mr-2 mt-4 mb-3">Elon</button>
+          <button onClick={this.handleKanye} type="button" className="btn btn-outline-primary btn-lg col-md-4 ml-2 mt-4 mr-2 mb-3">Kanye</button>
           <img src="https://cdn11.bigcommerce.com/s-balh3740/images/stencil/1280x1280/products/26297/46281/KanyeWest__85697.1538500006.jpg?c=2" className="img-fluid" alt="Responsive"  width="65px"></img>
 
         </Row>
--- twitterproj/src/components/tweet.js
+++ twitterproj/src/components/tweet.js
@@ -1,21 +1,23 @@
 import axios from 'axios';
 
 
-const Tweet = {
-    getTweet(personName) {
+const Tweet = { //object that will be exported
+    getTweet(personName) { //method of the object
       let config = {
-        headers: {
+        headers: { //web headers sent to API for auth
           authorization: "Bearer AAAAAAAAAAAAAAAAAAAAACX5HgEAAAAAyPFmFuUj3kPPH90SaeMwQoa%2FmFs%3DoNmiJejgc7yrqro6sv3YOqJFTavTAo4Y109R7a5sZTR4qw7q4Y", //Bearer token
         }
       }
 
       let filteredTweets = [];
 
+      //GET request made to API endpoint, bypassed CORS, and returned
       return axios.get(`https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${personName}&count=200`, config)
       .then(res => {
         const tweets = res.data;
+        //filtered for @ and web symbols
         filteredTweets = tweets.filter(tweet => {
-          if(tweet.text.includes("@") || tweet.text.includes("http")) { //the res.data.entities.urls and ...entities.user_mentions wasn't working for me so I did it manually
+          if(tweet.text.includes("&amp") || tweet.text.includes("@") || tweet.text.includes("http")) { //the res.data.entities.urls and ...entities.user_mentions wasn't working for me so I did it manually
             return null;
           }
           else {
@@ -23,7 +25,7 @@ const Tweet = {
           }
 
       })
-      //console.log(filteredTweets);
+      //returns the final object of tweets (it's 16 arrays for Elon at the current time)
       return filteredTweets;
 
   })
@@ -31,4 +33,5 @@ const Tweet = {
 }
 }
 
+//export the info so other classes can use this object
 export default Tweet;
